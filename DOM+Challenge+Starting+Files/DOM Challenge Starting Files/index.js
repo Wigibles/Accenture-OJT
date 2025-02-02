1cdf66bb2896695add218cd13eb0b8200ba6bc78
Wigibles <luigisantiago0723@gmail.com>
2025-02-02 11:30:57 +0800

    dice game

--- DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,4 @@
 document.querySelector('html').querySelector('body').querySelector('ul').lastElementChild.innerHTML="Mario";
+
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").innerHTML="<em>My website</em>";
